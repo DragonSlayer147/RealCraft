4f7bbcebdd7abbffbcdd4326b22d498b1d49ac26
TheFatherKaz <DragonSlayer147147@gmail.com>
2022-07-14 22:15:52 -0700

    Made cotton texture

--- src/main/java/com/realcraft/thefatherkaz/init/ItemInit.java
+++ src/main/java/com/realcraft/thefatherkaz/init/ItemInit.java
@@ -25,11 +25,19 @@ public class ItemInit {
 	public static final Item COPPER_INGOT = new MiscellaneousiBase("copper_ingot");
 	public static final Item RAW_COPPER = new MiscellaneousiBase("raw_copper");
 	public static final Item ROCK = new MiscellaneousiBase("rock");
+	public static final Item COTTON = new MiscellaneousiBase("cotton");
+	public static final Item MARIJUANA = new MiscellaneousiBase("marijuana");
+	public static final Item RAW_RICE = new MiscellaneousiBase("raw_rice");
+	public static final Item COFFEE_BEANS = new MiscellaneousiBase("coffee_beans");
 	
 	//Foodstuffs Tab
 	
 	public static final Item CORN = new FoodstuffsBase("corn", 3, false);
 	public static final Item POPCORN = new FoodstuffsBase("popcorn", 4, false);
+	public static final Item TOMATO = new FoodstuffsBase("tomato", 3, false);
+	public static final Item EGGPLANT = new FoodstuffsBase("eggplant", -1, false);
+	public static final Item LETTUCE = new FoodstuffsBase("lettuce", 1, false);
+	public static final Item PEANUTS = new FoodstuffsBase("peanuts", 1, false);
 	
 	//Tools Tab
 	
